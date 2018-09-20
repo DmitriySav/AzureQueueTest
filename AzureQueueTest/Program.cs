a2181b77c87f4225459b8400374d380c7931821b
DmitriySav <decky1994@gmail.com>
2018-09-20 22:55:47 +0300

    Добавьте файлы проекта.

---
+++ AzureQueueTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+
+namespace AzureQueueTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+               CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+            queue.CreateIfNotExists();
+            for (int i = 0; i < 10; i++)
+            {
+                queue.AddMessage(new CloudQueueMessage("Hello" + i));
+            }
+            queue.FetchAttributes();
+
+            // Retrieve the cached approximate message count.
+            int? cachedMessageCount = queue.ApproximateMessageCount;
+
+            // Display number of messages.
+            Console.WriteLine("Number of messages in queue: " + cachedMessageCount);
+            Console.ReadKey();
+        }
+    }
+}
